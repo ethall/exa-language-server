6904950971b3d823b91049bd2f2f1013c4a60fa7
Evan Hall <evan.tom.hall@gmail.com>
2022-05-31 13:07:01 -0400

    Update documents.rs
    Added text and version to the Document struct.

--- exa-language-server/src/documents.rs
+++ exa-language-server/src/documents.rs
@@ -4,17 +4,21 @@ use tree_sitter::{Parser, Tree};
 pub struct Document {
     pub uri: Url,
     pub parser: Parser,
+    pub text: String,
     pub tree: Tree,
+    pub version: i64,
 }
 impl Document {
-    pub fn new(uri: Url, text: String) -> Self {
+    pub fn new(uri: Url, version: i64, text: String) -> Self {
         let mut parser = Parser::new();
         parser.set_language(tree_sitter_exa::language()).unwrap();
-        let tree = parser.parse(text, None).unwrap();
+        let tree = parser.parse(text.clone(), None).unwrap();
         Self {
             uri,
             parser,
-            tree
+            text,
+            tree,
+            version
         }
     }
 }
