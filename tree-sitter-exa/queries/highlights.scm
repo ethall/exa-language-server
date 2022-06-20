48b10defc5f08dde1b070f6924bd2b73c3c9c89b
Evan Hall <evan.tom.hall@gmail.com>
2022-06-20 15:17:18 -0400

    Create highlights.scm

---
+++ tree-sitter-exa/queries/highlights.scm
@@ -0,0 +1,23 @@
+(exa
+    (_) @function
+    !comment)
+
+; REGISTERS
+;----------
+
+(register (_) @variable.builtin)
+
+; PRIMATIVES
+;-----------
+
+(binary_operator) @operator
+
+(eof) @keyword
+
+(label) @label
+
+(mrd) @keyword
+
+(number) @number
+
+(comment) @comment
